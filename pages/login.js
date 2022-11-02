01652c3a08ab8050351e8a26a7badd3788c9d270
hasindusithmin <hasindusithmin64@gmail.com>
2022-11-02 12:22:33 +0530

    Update page/api/login.js

--- pages/login.js
+++ pages/login.js
@@ -17,7 +17,7 @@ export default function Login() {
                         src="https://telegram.org/js/telegram-widget.js?21"
                         data-telegram-login="computerized_bot"
                         data-size="large"
-                        data-auth-url="https://computerize.vercel.app/api/login"
+                        data-auth-url="https://computerized.vercel.app/api/login"
                         data-request-access="write"
                         onLoad={() => {
                             document.getElementById('wrapper').appendChild(document.getElementById('widget'));
